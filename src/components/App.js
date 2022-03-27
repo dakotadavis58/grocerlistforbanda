81b912ef23a9ecaee6190554ec72f782a82b0088
Dakota Davis <davisdakota58@gmail.com>
2022-03-27 14:41:10 -0400

    changed css so on phoness no top margin

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,9 @@ const App = () => {
     <div className="container align-middle h-100 d-flex bg-info">
       <div className="col">
         <div className="row justify-content-center align-self-center">
-          <h1 className="text-center">Grocery List App for Aiden and Britt</h1>
+          <h1 className="text-center m-3">
+            Grocery List App for Aiden and Britt
+          </h1>
         </div>
         <Form />
       </div>
